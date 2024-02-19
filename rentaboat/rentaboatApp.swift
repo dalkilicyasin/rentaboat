36de3f8b434add451caca3d2bd449e6e19533ccd
dalkilicyasin <Yd123456+*>
2024-02-19 17:16:16 +0300

    Initial Commit

---
+++ rentaboat/rentaboatApp.swift
@@ -0,0 +1,17 @@
+//
+//  rentaboatApp.swift
+//  rentaboat
+//
+//  Created by yasin on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct rentaboatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
